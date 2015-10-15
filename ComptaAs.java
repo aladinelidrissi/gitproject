4c874a00c48af798116d52d41c0aefef01acae39
Aladin Idrissi <aladinelidrissi@gmail.com>
2015-10-16 00:02:05 +0200

    primer commit del projecte

---
+++ ComptaAs.java
@@ -0,0 +1,19 @@
+package net.aladinidrissi.ComptaA;
+
+public class ComptaAs {
+	
+	private final static String[] a ={"cap","peu","cap","ma","llengua","ma","cap","ma","llengua","cap","ma"};
+
+	public static void main(String[] args) {
+
+            int comptador = 0;
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] == "ma") {
+                comptador++;
+                }
+            }
+            System.out.println("Numero de mans: " + comptador);
+
+	}
+
+}
